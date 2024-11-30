7de714e7e7a12b3eaaf6608c76338173a06f8674
fariselbattahe <fariselbattahe@gmail.com>
2024-11-30 01:17:18 -0500

    Hello Cydeo Class

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -1,5 +1,9 @@
 package day01;
-
+/*
+red color -->saved in working Directory
+green color --> saved in Staging Area
+white color --> saved in Local Repository [
+ */
 public class HelloWorld {
     public static void main(String[] args) {
 
