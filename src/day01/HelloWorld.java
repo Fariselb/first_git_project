9359c04d8b34d10cbc6a572bc7055fb68c61b49d
fariselbattahe <fariselbattahe@gmail.com>
2024-11-30 01:33:57 -0500

    Hello Cydeo Class

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,9 +3,11 @@ package day01;
 red color -->saved in working Directory
 green color --> saved in Staging Area
 white color --> saved in Local Repository [
+blue color --> (working directory)when you add/make code/change in the file ex: System.out.println("Hello World"); the file name become bleu, and this change has not been saved in the staging area then also the local repository.
  */
 public class HelloWorld {
     public static void main(String[] args) {
 
+        System.out.println("Hello World");
     }
 }
