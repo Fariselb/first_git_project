9f51a63c4b4a073f7e3cfe0e42d3b6b5338d1594
Fariselb <fariselbattahe@gmail.com>
2024-12-14 21:48:37 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -12,5 +12,6 @@ public class HelloWorld {
          System.out.println("Hello Cydeo"); // Updat made in remote repositorie.
          System.out.println("Hello Program"); // Updat made in remote repositorie.
         // Extrat review -> good 
+        // second test for fetch
     }
 }
